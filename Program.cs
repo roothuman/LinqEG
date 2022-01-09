c55635d25a2146debf4ef46a094108ab623c182f
Moses Abah <amoses@swiftHQ.com>
2022-01-09 10:05:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace LinqEG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var query = new DirectoryInfo(path).GetFiles()
+                .OrderByDescending(f => f.Length)
+                .Take(5);
+
+            foreach (var file in query)
+            {
+                Console.WriteLine($"{file.Name, -20} : {file.Length, -10:N0}");
+            }
+        }
+    }
+}
